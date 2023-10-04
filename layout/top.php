870a803643890e7b3d9ffff7a3dd6a9e7b0b338d
trunghieu26 <trunghieuqt26@gmail.com>
2023-10-04 22:07:50 +0700

    first commit

---
+++ layout/top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LIKEA FAUCET</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="../media/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../media/css/left-sidebar.css">
+    <link rel="stylesheet" href="../media/css/common.css">
+</head>
+<body>
+    <div class="wrapper">
+        <!-- <span><?php echo 'layout/left-sidebar.php' ?> </span> -->
+        <?php include_once '../layout/left-sidebar.php'; ?>
+    </div>

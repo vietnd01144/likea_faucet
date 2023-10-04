ac1bbb491f265165a7d57b5f2fd695b8a825b216
truongnhat24 <truongnhat2492@gmail.com>
2023-10-04 22:26:14 +0700

    Add font size

--- layout/top.php
+++ layout/top.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>LIKEA FAUCET</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../media/css/bootstrap.min.css">
     <link rel="stylesheet" href="../media/css/left-sidebar.css">
     <link rel="stylesheet" href="../media/css/common.css">
 </head>
+
 <body>
     <div class="wrapper">
         <!-- <span><?php echo 'layout/left-sidebar.php' ?> </span> -->

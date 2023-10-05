0d455a3ecad2ec2b1cdf30e54ab2ffb711f7c968
trunghieu26 <trunghieuqt26@gmail.com>
2023-10-05 16:37:48 +0700

    update code page daily quests

---
+++ layout/chat-main.php
@@ -0,0 +1,156 @@
+<section class="chat-main container">
+    <div class="chat-main-side">
+        <div class="user-online">
+            <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_i_1396_39671)">
+                    <circle cx="5.5" cy="5.5" r="5.5" fill="url(#paint0_linear_1396_39671)" />
+                </g>
+                <circle cx="5.5" cy="5.5" r="5" stroke="url(#paint1_linear_1396_39671)" />
+                <defs>
+                    <filter id="filter0_i_1396_39671" x="0" y="0" width="11" height="13" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dy="2" />
+                        <feGaussianBlur stdDeviation="1" />
+                        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                        <feColorMatrix type="matrix" values="0 0 0 0 0.545833 0 0 0 0 1 0 0 0 0 0.6512 0 0 0 1 0" />
+                        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_1396_39671" />
+                    </filter>
+                    <linearGradient id="paint0_linear_1396_39671" x1="10.5175" y1="11" x2="-0.707875" y2="10.2227" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#28A745" />
+                        <stop offset="1" stop-color="#5ED078" />
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_1396_39671" x1="0.192983" y1="-3.69265e-06" x2="7.10391" y2="0.899342" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" stop-opacity="0.5" />
+                        <stop offset="1" stop-color="white" stop-opacity="0" />
+                    </linearGradient>
+                </defs>
+            </svg>
+            <span class="fw-900 fs-13 text-white number-user">25</span>
+            <span class="fw-500 fs-13 player-online">Player Online</span>
+        </div>
+        <div class="list-message">
+            <div class="coutdown-time-chat">
+                <div class="icon-coutdown-time">
+                    <img src="../media/image/count-down.svg">
+                </div>
+                <div class="list-chat">
+                    <div class="content-coutdown">
+                        <span class="fw-bolder text-white fs-18 lh-24 title-content-coutdown">Chat Rain</span>
+                        <span class="body-content-coutdown fw-500 fs-13 lh-18">Randomly reward players in chat room every <span class="fw-900"> 5 Hour!</span></span>
+                        <div class="fs-26 fw-bolder">
+                            <span class="text-white">
+                                08
+                            </span>
+                            <span class="text-gray-daily"> h : </span>
+                            <span class="text-white">
+                                43
+                            </span>
+                            <span class="text-gray-daily"> m : </span>
+                            <span class="text-white">
+                                23
+                            </span>
+                            <span class="text-gray-daily"> s </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/icon-user.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">KTT Darling</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Hey! i will be glad to new accquaintances! 👋</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/jenny-wilson.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">Jenny Wilson</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget risus nec quam ultricies egestas ut vel est. Fusce suscipit dolor in ex suscipit molestie.</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/devon-lane.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">Devon Lane</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Morbi enim quam, pulvinar eu risus iaculis, mattis luctus</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/flores.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">Albert Flores</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Hey! i will be glad to new accquaintances! 😈😈😈</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/russell.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">Dianne Russell</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Vestibulum quis libero lectus. Nulla rhoncus mi in augue tempus ultrices 😮‍💨😮‍💨</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/mcoy.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">Arlene McCoy</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Hey! i will be glad to new accquaintances! 👋</span>
+                </div>
+            </div>
+            <div class="message-chat mt-12">
+                <div class="icon-user">
+                    <img src="../media/image/rioclass.svg">
+                </div>
+                <div class="body-message">
+                    <div class="name-time-message">
+                        <span class="fw-bolder">RioClass 17</span>
+                        <span>2:13Pm</span>
+                    </div>
+                    <span class="body-content-coutdown fw-500 fs-13 lh-18">Proin aliquam lacus vitae nulla sollicitudin condimentum.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="input-send-mess">
+        <input class="input-message" placeholder="Send A Messager" value="">
+        <a class="icon-send">
+            <svg width="1" height="20" viewBox="0 0 1 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="1" height="20" fill="#242C48" />
+            </svg>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.9412 3.85159L16.9234 16.3047C16.6957 17.1836 16.1019 17.4024 15.2582 16.9883L10.66 14.0235L8.44124 15.8906C8.1957 16.1055 7.99034 16.2852 7.51713 16.2852L7.84749 12.1875L16.3698 5.44924C16.7403 5.16018 16.2894 5.00002 15.7939 5.28909L5.2582 11.0938L0.722485 9.85159C-0.264122 9.58205 -0.281979 8.9883 0.927842 8.57424L18.6689 2.59377C19.4903 2.32424 20.2091 2.75393 19.9412 3.85159Z" fill="#8691B2" />
+            </svg>
+        </a>
+    </div>
+</section>
--- layout/top.php
+++ layout/top.php
@@ -10,8 +10,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../media/css/bootstrap.min.css">
     <link rel="stylesheet" href="../media/css/common.css">
+    <link rel="stylesheet" href="../media/css/common.css">
     <link rel="stylesheet" href="../media/css/left-sidebar.css">
     <link rel="stylesheet" href="../media/css/main-page.css">
+    <link rel="stylesheet" href="../media/css/header.css">
+    <link rel="stylesheet" href="../media/css/chat-main.css">
 </head>
 
 <body>
@@ -20,16 +23,14 @@
             <div class="col-2 px-0">
                 <?php include_once '../layout/left-sidebar.php'; ?>
             </div>
-    
-            <!-- <span><?php echo 'layout/left-sidebar.php' ?> </span> -->
-    
             <section class="main-container col-10">
                 <div class="row">
-                    <div class="col-8">
+                    <?php include_once '../layout/header.php'; ?>
+                    <div class="col-9">
                         <?php include_once '../layout/main.php'; ?>
                     </div>
-                    <div class="col-4">
-        
+                    <div class="col-3 right-chat">
+                        <?php include_once '../layout/chat-main.php'; ?>
                     </div>
                 </div>
             </section>

bf0d3b874ff6ef32b4512b5028270e6781bd0b47
truongnhat24 <truongnhat2492@gmail.com>
2023-10-05 10:44:55 +0700

    update - body

---
+++ layout/main.php
@@ -0,0 +1,78 @@
+<section class="main container">
+  <div>
+    <img src="../media/image/banner_main.png" alt="" class="w-100">
+  </div>
+
+  <div class="daily-action ">
+    <div class="daily-action-group d-flex justify-content-between">
+      <div class="daily-countdown">
+        <span class="text-white">Daily Countdown:</span>
+        <div class="fs-26 fw-bolder">
+          <span class="text-orange-daily">
+            08
+          </span>
+          <span class="text-gray-daily"> h : </span>
+          <span class="text-orange-daily">
+            43
+          </span>
+          <span class="text-gray-daily"> m : </span>
+          <span class="text-orange-daily">
+            23
+          </span>
+          <span class="text-gray-daily"> s </span>
+        </div>
+      </div>
+  
+      <div class="daily-quest">
+        <span class="text-white">Complete All Quests</span>
+        <div>
+  
+        </div>
+      </div>
+  
+      <div class="daily-gift d-flex">
+        <div>
+          <img src="../media/image/daily_gift.png" alt="">
+        </div>
+        <div>
+          <span class="text-white">
+            Pick Up
+          </span>
+          <div class="fw-bolder">
+            <span class="daily-coins">500 Coins</span>
+            <span class="text-white">+</span>
+            <span class="daily-exp">50 Exp</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-option">
+    <div class="row">
+      <div class="col-xl-3">
+        <div>
+          <div>
+            <img src="../media/image/item1.png" alt="">
+          </div>
+          <div>
+            <span>
+              Solve 10 Shortlinks
+            </span>
+            <div>
+              8/10
+            </div>
+            <div class="d-flex">
+              <span>
+                5 Coins 
+              </span>
+              <div>
+                <img src="../media/image/bronze_coin.svg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
--- layout/top.php
+++ layout/top.php
@@ -9,12 +9,29 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../media/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../media/css/left-sidebar.css">
     <link rel="stylesheet" href="../media/css/common.css">
+    <link rel="stylesheet" href="../media/css/left-sidebar.css">
+    <link rel="stylesheet" href="../media/css/main-page.css">
 </head>
 
 <body>
-    <div class="wrapper">
-        <!-- <span><?php echo 'layout/left-sidebar.php' ?> </span> -->
-        <?php include_once '../layout/left-sidebar.php'; ?>
-    </div>
+    <section class="container-fluid">
+        <section class="wrapper row">
+            <div class="col-2 px-0">
+                <?php include_once '../layout/left-sidebar.php'; ?>
+            </div>
+    
+            <!-- <span><?php echo 'layout/left-sidebar.php' ?> </span> -->
+    
+            <section class="main-container col-10">
+                <div class="row">
+                    <div class="col-8">
+                        <?php include_once '../layout/main.php'; ?>
+                    </div>
+                    <div class="col-4">
+        
+                    </div>
+                </div>
+            </section>
+        </section>
+    </section>
